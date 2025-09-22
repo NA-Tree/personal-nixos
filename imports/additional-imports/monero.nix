7b12efb006305b791acce619b25019f43b69ff6d
ostorms <ostorms@sensonix.com>
2025-09-22 08:10:02 -0600

    added config.json for xmrig but it is still untested

--- imports/additional-imports/monero.nix
+++ imports/additional-imports/monero.nix
@@ -5,20 +5,23 @@
 
         enable = true;
 
-        settings = {
-            autosave = true;
-            cpu = true;
-            opencl = false;
-            cuda = false;
-            pools = [
-                {
-                url = "pool.supportxmr.com:443";
-                user = "your-wallet";
-                keepalive = true;
-                tls = true;
-                }
-            ]
-        }
+        settings = builtins.fromJSON( builtins.readFile( ./resources/mining.json ) );
+        
+        # {
+
+        #     autosave = true;
+        #     cpu = true;
+        #     opencl = false;
+        #     cuda = false;
+        #     pools = [
+        #         {
+        #         url = "pool.supportxmr.com:443";
+        #         user = "your-wallet";
+        #         keepalive = true;
+        #         tls = true;
+        #         }
+        #     ]
+        # }
     }
 
-}
+}m.js
